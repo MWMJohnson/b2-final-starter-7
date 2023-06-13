bba8a1a3d1a94b55a69985f9e71843746ff2fb9b
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-06-13 13:31:34 -0600

    Coupon: add uniqueness validation to code.

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -4,13 +4,14 @@ class Coupon < ApplicationRecord
                         :status,
                         :discount_type,
                         :discount_value
+  validates :code, uniqueness: :true
 
                       
   belongs_to :merchant
   has_many :invoices
   has_many :invoice_items, through: :invoices
 
-  enum status: [:inactive, :active]
+  enum status: [:deactivated, :activated]
   enum discount_type: [:dollar_off, :percent_off]
 
 
