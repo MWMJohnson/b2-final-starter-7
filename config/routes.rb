66431c59e30bd48daacf004c2f0d5b8978aaad59
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-06-13 13:28:57 -0600

    Feat: coupon_status#update route.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :item_status, only: [:update]
     resources :invoices, only: [:index, :show, :update]
     resources :coupons, except: [:destroy]
+    resources :coupon_status, only: [:update]
+
 
   end
 
