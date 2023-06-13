14c7c3b50fe3b01a64fd831da5e95bb7708b376e
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-06-13 13:26:24 -0600

    Test: validates uniqueness for :code.

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
+
+
 RSpec.describe Coupon, type: :model do
+  before(:each) do 
+    @merchant1 = Merchant.create!(name: "Hair Care")
+    @coupon_1 = @merchant1.coupons.create!(name: "5% off!", code: "5-P", status: 0, discount_type: 1, discount_value: 5)
+  end
+
   describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_presence_of :code }
     it { should validate_presence_of :status }
     it { should validate_presence_of :discount_type }
     it { should validate_presence_of :discount_value }
-
+    it { should validate_uniqueness_of(:code) }
   end
 
   describe "relationships" do
